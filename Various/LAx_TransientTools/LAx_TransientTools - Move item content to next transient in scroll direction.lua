7e30ecd173c4d7d79daa4ce6e1b662c529d4c50d
Leon Beilmann <Leon.Beilmann@freenet.de>
2025-07-31 14:43:26 +0200

    Added: "LAx_TransientTools - Move item content to next transient in scroll direction.lua" allowing to scroll through transients

--- Various/LAx_TransientTools/LAx_TransientTools - Move item content to next transient in scroll direction.lua
+++ Various/LAx_TransientTools/LAx_TransientTools - Move item content to next transient in scroll direction.lua
@@ -1,23 +1,11 @@
--- @description LAx_TransientTools
--- @author Leon 'LAxemann' Beilmann
--- @version 1.01
--- @about
---   # About
---   Simple scripts for transient-based workflows
---
---  # Requirements
---  JS_ReaScriptAPI, SWS Extension
--- @links
---  Website https://www.youtube.com/@LAxemann
--- @provides
---   [main] LAx_TransientTools - Move item content to previous transient.lua
---   [main] LAx_TransientTools - Move item content to next transient in scroll direction.lua
---   [nomain] runShared.lua
+-- @noindex
+
 --[[
- * Changelog:
-    * v1.01
-      + Added: "LAx_TransientTools - Move item content to next transient in scroll direction.lua" allowing to scroll through transients
-]] ----------------------------------------------------------------------------------------
+    This version of the script can be assigned to a "scrollable" key such as the mousewheel.
+    The transient detection will then depend on the scroll direction.
+--]]
+
+----------------------------------------------------------------------------------------
 -- Run Shared
 LAx_Shared_Installed = false
 local currentFolder = (debug.getinfo(1).source:match("@?(.*[\\|/])"))
@@ -32,23 +20,43 @@ end
 -- Requirements
 local transients = require("LAx_Shared_Transients")
 local utility = require("LAx_Shared_Utility")
+local extState = require("LAx_Shared_ExtState")
 
 if not utility.checkRequiredExtensions("LAx_TransientTools", { "JS_VKeys_GetState", "CF_GetSWSVersion" }) then
     return
 end
 
 ----------------------------------------------------------------------------------------
--------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
+local newValue, _, _, _, _, _, val, contextStr = reaper.get_action_context()
+
+if val == 0 or not newValue then
+    return
+end
+
+if string.match(contextStr, "hwheel") then
+    local scrollDelayMin = 0.225
+    local lastScroll = tonumber(extState.getExtStateValue(LAx_ProductData.name, "LastScroll", "0"))
+
+    if os.clock() - lastScroll < scrollDelayMin then
+        return
+    end
+
+    reaper.SetExtState(LAx_ProductData.name, "LastScroll", tostring(os.clock()), false)
+end
+
 local selectedItemCount = reaper.CountSelectedMediaItems(0)
 
 if selectedItemCount == 0 then
     return
 end
 
+local direction = val > 0 and 1 or -1
+
 for i = 0, selectedItemCount - 1 do
     local item = reaper.GetSelectedMediaItem(0, i)
 
     if item then
-        transients.moveItemContentToNextTransient(item, 1)
+        transients.moveItemContentToNextTransient(item, direction)
     end
 end
